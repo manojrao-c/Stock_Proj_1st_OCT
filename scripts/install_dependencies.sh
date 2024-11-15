11943844987edbdb8af191adff74ffac02e78b62
DarbhaPreetham <preetham.darbha@gmail.com>
2024-11-15 15:45:43 +0530

    Updated install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,4 +2,4 @@
 sudo yum update -y
 sudo yum install -y python3 python3-pip
 pip3 install --upgrade pip
-pip3 install -r /home/ec2-user/Stock_Proj_1st_OCT/requirements.txt
+pip3 install --ignore-installed -r /home/ec2-user/Stock_Proj_1st_OCT/requirements.txt
